9160a0f72e92c54b3bc7df78d19dfb8560750dee
abhimanyunaikare <abhimanyu.naikare@gmail.com>
2026-06-30 12:07:27 +0530

    first commit - changes to port

--- src/index.js
+++ src/index.js
@@ -33,4 +33,4 @@ app.post('/api/webhook/razorpay', express.raw({ type: 'application/json' }), (re
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅  SY Events backend running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`✅  SY Events backend running on port ${PORT}`));
